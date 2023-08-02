c6ad5dfb9a8cb937ac70fda8bb8d4fecb0ff02fb
ZARIFMAC <uzbzarif@gmail.com>
2023-08-02 15:03:30 +0500

    get data from api

--- lib/user.dart
+++ lib/user.dart
@@ -1,10 +1,39 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 class User {
-  final String firstName;
-  final String lastName;
-  final String email;
+  String firstName;
+  String lastName;
+  String email;
   User({
     required this.firstName,
     required this.lastName,
     required this.email,
   });
+  // Set the first name
+  set setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  void info() {
+    Uri url = Uri.parse('https://randomuser.me/api');
+    http.get(url).then((response) {
+      if (response.statusCode == 200) {
+        Map body = json.decode(response.body);
+        String firstName = body['results'][0]['name']['first'];
+        // Get the last name
+        String lastName = body['results'][0]['name']['last'];
+        // Get the email address
+        String email = body['results'][0]['email'];
+        print('First Name: $firstName');
+        print('Last Name: $lastName');
+      }
+    });
+    print('First Name: $firstName');
+    print('Last Name: $lastName');
+    print('Email: $email');
+  }
+
+  // Get the first name
+  String get getFirstName => firstName;
 }
